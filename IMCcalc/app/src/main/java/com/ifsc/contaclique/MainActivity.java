d74dd9c28929592200cad0e462820837f9ede5d1
Felipe <felipe.w28@aluno.ifsc.edu.br>
2025-03-31 07:58:20 -0300

    Numero formatado
    formatando o numero imc

--- IMCcalc/app/src/main/java/com/ifsc/contaclique/MainActivity.java
+++ IMCcalc/app/src/main/java/com/ifsc/contaclique/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
 
     int i=0;
@@ -33,8 +35,10 @@ public class MainActivity extends AppCompatActivity {
             peso = Double.parseDouble(edpeso.getText().toString());
             altura = Double.parseDouble(edaltura.getText().toString());
             imc = peso/altura;
+            //Formatando 00,00
 
-            tvresultado.setText(Double.toString(imc));
+            DecimalFormat decimalformat=new DecimalFormat("##.##");
+            tvresultado.setText(decimalformat.format(imc));
         });
 
     }
