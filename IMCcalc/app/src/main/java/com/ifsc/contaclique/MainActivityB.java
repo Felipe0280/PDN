91d3a8c1da69648d7547a0ad390516a5bc185bd9
felipe <felipe.w28@aluno.ifsc.edu.br>
2025-04-07 08:50:17 -0300

    SlideShow
    com problemas no imc, e muitos problemas

--- IMCcalc/app/src/main/java/com/ifsc/contaclique/MainActivityB.java
+++ IMCcalc/app/src/main/java/com/ifsc/contaclique/MainActivityB.java
@@ -1,25 +1,28 @@
 package com.ifsc.contaclique;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivityB extends AppCompatActivity {
-
+    ImageView imageview;
+    Integer[] imagens= new Integer[]{
+            R.drawable.spinosaurusRugindo
+    };
+    int posicao = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_b);
 
-
             Bundle bundle= getIntent().getExtras();
             String msg= bundle.getString("mensaqem");
             TextView txtView = findViewById(R.id.txtAtt2);
             txtView.setText(msg);
+
+           // imageview.setImageResource(imagens(posicao));
+
         };
     }
